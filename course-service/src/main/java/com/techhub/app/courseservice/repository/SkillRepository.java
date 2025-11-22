7aec51b297635e10cfffc0f0f9817e78161de251
tuyen <tuyenbest1234@gmail.com>
2025-11-22 12:55:44 +0700

    fix(CourseService) : tag vs skill

--- course-service/src/main/java/com/techhub/app/courseservice/repository/SkillRepository.java
+++ course-service/src/main/java/com/techhub/app/courseservice/repository/SkillRepository.java
@@ -2,7 +2,9 @@ package com.techhub.app.courseservice.repository;
 
 import com.techhub.app.courseservice.entity.Skill;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface SkillRepository extends JpaRepository<Skill, UUID> {
+    Optional<Skill> findByName(String name);
 }
--- course-service/src/main/java/com/techhub/app/courseservice/repository/TagRepository.java
+++ course-service/src/main/java/com/techhub/app/courseservice/repository/TagRepository.java
@@ -2,7 +2,9 @@ package com.techhub.app.courseservice.repository;
 
 import com.techhub.app.courseservice.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TagRepository extends JpaRepository<Tag, UUID> {
+    Optional<Tag> findByName(String name);
 }
