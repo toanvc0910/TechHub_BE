5be59e790308ae930e103da161131bde257253e7
tuyen <tuyenbest1234@gmail.com>
2025-11-26 18:32:10 +0700

    config(CourseService) : config kafka to get from common

--- course-service/src/main/java/com/techhub/app/courseservice/CourseserviceApplication.java
+++ course-service/src/main/java/com/techhub/app/courseservice/CourseserviceApplication.java
@@ -5,14 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients(basePackages = "com.techhub.app.courseservice.client")
 @ComponentScan(basePackages = {
-    "com.techhub.app.courseservice",
-    "com.techhub.app.commonservice"
-})
+		"com.techhub.app.courseservice",
+		"com.techhub.app.commonservice"
+}, excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "com\\.techhub\\.app\\.commonservice\\.kafka\\..*"))
 public class CourseserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CourseserviceApplication.class, args);
